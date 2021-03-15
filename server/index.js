7489d570419119131c7ad97a6f563e436dc80ff5
Jorge Martin <jorgemaram@gmail.com>
2021-03-15 11:52:16 +0100

    creating the server environment

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+
+//crear servidor
+const app = express();
+
+//Conectar DB
+conectarDB();
+
+//Puerto de la app
+const port = process.env.PORT || 4000;
+
+//Rutas de la app
+app.use('/api/usuarios', require('./routes/usuarios'));
+
+
+//Arrancar la app
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor está funcionando en el puerto ${port}`)
+} )
