4e7a2a6f560840a1f46fa540c76742fadd0c977e
Jorge Martin <jorgemaram@gmail.com>
2021-03-16 14:15:47 +0100

    creation of middleware and use of JWT

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,16 @@
 const Usuario = require('../models/Usuario');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
+const { validationResult } = require('express-validator');
+
 
 exports.autenticarUsuario = async (req, res, next) => {
     //Revisar si hay errores
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ errores: errores.array() });
+    }
 
     //Buscar el usuario para ver si está registrado
     const { email, password } = req.body;
@@ -17,7 +25,15 @@ exports.autenticarUsuario = async (req, res, next) => {
     //Verificar el password y autenticar el usuario
     if (bcrypt.compareSync(password, usuario.password)) {
         //Crear JWT
-        
+        const token = jwt.sign({
+            id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email
+        }, process.env.SECRETA, {
+            expiresIn: '8h'
+        });
+        res.json({token});
+
     } else {
         res.status(401).json({ msg: "Password incorrecto" });
         return next();
@@ -25,6 +41,6 @@ exports.autenticarUsuario = async (req, res, next) => {
 
 }
 
-exports.usuarioAutenticado = (req, res) => {
-
+exports.usuarioAutenticado = (req, res, next) => {
+    res.json({usuario: req.usuario});
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { check } = require('express-validator');
+const auth = require('../middleware/auth');
 
 router.post('/',
+    [
+        check('email', 'Agrega un email válido').isEmail(),
+        check('password', 'El password no puede ser un campo vacío').not().isEmpty()
+    ],
     authController.autenticarUsuario
 );
 
 router.get('/',
+    auth,
     authController.usuarioAutenticado
 );
 
