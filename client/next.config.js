9bcd72de377d2ed921ab46349d0d79fd8b4a3a91
Jorge Martin <jorgemaram@gmail.com>
2021-03-23 12:25:12 +0100

    frontend deployment final

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        backendURL: 'http://localhost:4000',
-        frontendURL: 'http://localhost:3000'
+        backendURL: 'https://node-send-server.herokuapp.com/',
+        frontendURL: 'https://node-send-c-nxf1j670m-jorgemaram.vercel.app/'
     }
 }
